e847700f067e6dcba9affdba4f52985fcbc5a707
tobby340602 <niklaussergei23@gmail.com>
2024-05-15 00:09:27 +1000

    changed getprice function

--- src/getPrice.ts
+++ src/getPrice.ts
@@ -1,10 +1,17 @@
+import { get } from "http";
+
 export const getPrice = async (tokenMint: string) => {
 
-    const res: any = await fetch(`https://api.dexscreener.com/latest/dex/tokens/${tokenMint}`);
-    const data = await res.json();
-    const price = data.pairs.find((x: any) => x.priceNative).priceNative
-    // console.log(`- Token: ${tokenMint}, Price: ${price}`);
-    return Number(price);
+    try {
+        const res: any = await fetch(`https://api.dexscreener.com/latest/dex/tokens/${tokenMint}`);
+        const data = await res.json();
+        const price = data.pairs.find((x: any) => x.priceNative).priceNative
+        // console.log(`- Token: ${tokenMint}, Price: ${price}`);
+        return Number(price);
+    }
+    catch (e) {
+        getPrice(tokenMint);
+    }
 }
 // const token = 'tgw7RiFwLUsM9dHNGj2mdAZjuivdsk8FH9RESxFX8LX'
 // getPrice(token);
