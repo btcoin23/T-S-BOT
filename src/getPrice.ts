eae24234da1e7d69bfd36bd61922a03aaad9431b
tobby340602 <niklaussergei23@gmail.com>
2024-05-15 06:28:21 +1000

    e

--- src/getPrice.ts
+++ src/getPrice.ts
@@ -6,7 +6,7 @@ export const getPrice = async (tokenMint: string) => {
         const res: any = await fetch(`https://api.dexscreener.com/latest/dex/tokens/${tokenMint}`);
         const data = await res.json();
         const price = data.pairs.find((x: any) => x.priceNative).priceNative
-        // console.log(`- Token: ${tokenMint}, Price: ${price}`);
+        console.log(`- Token: ${tokenMint}, Price: ${price}`);
         return Number(price);
     }
     catch (e) {
