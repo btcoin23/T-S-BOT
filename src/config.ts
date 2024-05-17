08d3dc6a62487db54d9a459e43fd4a760602ff4e
tobby340602 <niklaussergei23@gmail.com>
2024-05-18 01:12:20 +1000

    1

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ import 'dotenv/config';
 
 //---------------------------------------------------------------- User setting
 export const BotConfig = {
-    trackWallet: 'FjEKhdhqskSHUgkTxuDA55WoynHx6UyCxxS7caX1QXJi',
+    trackWallet: 'A1xmSqF17k2kenQH3hu9m9CTcKAAMauQfjqE7ab2cvKB',
         // 'N22uCb2j9GriychZJzh9t5idaddLdkHhANNH27GUPYC',
         // 'FjEKhdhqskSHUgkTxuDA55WoynHx6UyCxxS7caX1QXJi',
     threshold: 1000,// 1000 SOL
